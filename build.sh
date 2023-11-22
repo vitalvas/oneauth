25ba1067a2c281df7aa5be0cc14d0c2d2544a369
Vitaliy Vasylenko <me@vitalvas.com>
2023-11-22 16:52:43 -0500

    fix: pack archive

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ mkdir -p build
 
 GOOS=darwin GOARCH=amd64 go build -ldflags '-w -s' -o build/oneauth_darwin_amd64 cmd/oneauth/main.go
 
-tar -czvf build/oneauth_darwin_amd64.tar.gz build/oneauth_darwin_amd64
+tar -czvf build/oneauth_darwin_amd64.tar.gz -C build/ oneauth_darwin_amd64
 
 if [ ! -z "${GITHUB_ACTIONS}" ]; then
     aws s3 cp build/oneauth_darwin_amd64.tar.gz s3://vv-github-build-artifacts/${GITHUB_REPOSITORY}/${GITHUB_REF_NAME}/${GITHUB_SHA}/oneauth_darwin_amd64.tar.gz
