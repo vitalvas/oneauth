e47c46b8cec25e29d3dd12845524b02f14705392
Vitaliy Vasylenko <me@vitalvas.com>
2023-11-21 19:25:38 -0500

    fix: build for osx

--- build.sh
+++ build.sh
@@ -9,5 +9,5 @@ GOOS=darwin GOARCH=amd64 go build -ldflags '-w -s' -o build/oneauth_darwin_amd64
 tar -czvf build/oneauth_darwin_amd64.tar.gz build/oneauth_darwin_amd64
 
 if [ ! -z "${GITHUB_ACTIONS}" ]; then
-    aws s3 cp oneauth_darwin_amd64.tar.gz s3://vv-github-build-artifacts/${GITHUB_REPOSITORY}/${GITHUB_REF_NAME}/${GITHUB_SHA}/oneauth_darwin_amd64.tar.gz
+    aws s3 cp build/oneauth_darwin_amd64.tar.gz s3://vv-github-build-artifacts/${GITHUB_REPOSITORY}/${GITHUB_REF_NAME}/${GITHUB_SHA}/oneauth_darwin_amd64.tar.gz
 fi
