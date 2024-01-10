0c4d0404db5a0f06c514dd8ee925774e308d0dbf
Vitaliy Vasylenko <me@vitalvas.com>
2024-01-09 22:20:40 -0500

    fix(ci): correct upload path

--- build.py
+++ build.py
@@ -17,7 +17,7 @@ class Make:
         self._commit_id = os.getenv('GITHUB_SHA')
         self.GOOS = self._exec('go env GOOS')
         self.GOARCH = self._exec('go env GOARCH')
-        self._apps = [
+        self.apps = [
             {
                 'name': 'oneauth',
                 'dir': 'cmd/oneauth',
@@ -42,7 +42,7 @@ class Make:
                 ]
             }
         ]
-    
+
     @staticmethod
     def _get_version() -> str:
         # on create tag
@@ -56,7 +56,7 @@ class Make:
             return 'v0.0.' + run_id
 
         return 'v0.0.' + str(int(time.time()))
-        
+
     @staticmethod
     def _exec(cmd: str) -> str:
         stream = os.popen(cmd)
@@ -69,28 +69,27 @@ class Make:
 
         os.mkdir('./build')
 
-    def build_app(self, conf: dict) -> list:
+    def build_app(self, conf: dict) -> dict:
         app_name = conf.get('name')
         app_dir = conf.get('dir')
         
         print(f'-- Building {app_name}...')
-        
-        upload_files = []
-        
+
+        upload_files = {}
+
         matrix = conf.get('matrix')
         if not matrix:
-            upload_files.extend(
+            upload_files.update(
                 self.build_bin(app_name, self.GOOS, self.GOARCH, '1', app_dir)
             )
         else:
             for env in matrix:
                 files = self.build_bin(app_name, env.get('GOOS'), env.get('GOARCH'), env.get('CGO_ENABLED'), app_dir)
-                upload_files.extend(files)
+                upload_files.update(files)
 
         return upload_files
-                
-        
-    def build_bin(self, name: str, goos: str, goarch: str, cgo: str, app_dir: str) -> list:
+
+    def build_bin(self, name: str, goos: str, goarch: str, cgo: str, app_dir: str) -> dict:
         my_env = os.environ.copy()
         my_env['CGO_ENABLED'] = cgo
         my_env['GOOS'] = goos
@@ -104,7 +103,7 @@ class Make:
         output = f'./build/{goos}/{goarch}/{name}'
 
         build_cmd = ['go', 'build', '-ldflags', ld_flags, '-o', output, f'{app_dir}/main.go']
-        
+
         raw = subprocess.Popen(
             build_cmd,
             env=my_env,
@@ -114,25 +113,24 @@ class Make:
 
         for line in raw.stdout:
             print(line.decode('utf-8').strip())
-            
+
         is_break = False
         for line in raw.stderr:
             print(line.decode('utf-8').strip())
             is_break = True
-            
+
         if is_break:
             sys.exit(1)
-            
-        upload_files = [
-            self.create_archive(name, goos, goarch),
-            self.create_manifest(name, goos, goarch)
-        ]
-        
+
+        upload_files = {}
+        upload_files.update(self.create_archive(name, goos, goarch))
+        upload_files.update(self.create_manifest(name, goos, goarch))
+
         _update_manifest = self.create_update_manifest(name)
         ref_name = os.getenv('GITHUB_REF_NAME')
         if ref_name and (ref_name == 'master' or ref_name.startswith('v')):
-            upload_files.append(_update_manifest)
-        
+            upload_files.update(_update_manifest)
+
         return upload_files
 
     def create_archive(self, name: str, goos: str, goarch: str) -> str:
@@ -154,16 +152,16 @@ class Make:
         }
         if self._commit_id:
             manifest['commit'] = self._commit_id
-        
+
         file_name = f'{name}_{goos}_{goarch}_manifest.json'
-        
+
         with open(f'build/{file_name}', 'w') as file:
             json.dump(manifest, file)
 
         return {
             file_name: f'{self.VERSION}/{file_name}'
         }
-    
+
     def create_update_manifest(self, name: str) -> dict:
         repo = os.getenv('GITHUB_REPOSITORY', 'vitalvas/oneauth')
         manifest = {
@@ -171,42 +169,42 @@ class Make:
             'version': self.VERSION,
             'remote_prefix': f'https://github-build-artifacts.vitalvas.dev/{repo}/',
         }
-        
+
         if self.RELEASE:
             manifest['remote_prefix'] = 'https://oneauth-files.vitalvas.dev/release/'
-            
+
         file_name = f'{name}_update_manifest.json'
-        
+
         with open(f'build/{file_name}', 'w') as file:
             json.dump(manifest, file)
-        
+
         return {
             file_name: file_name
         }
 
-    def upload_files(self, files: list) -> None:
+    def upload_files(self, files: dict) -> None:
         print('Uploading files...')
-        for file in files:
-            self.upload_file(file)
+        for src, dst in files.items():
+            self.upload_file(src, dst)
 
-    def upload_file(self, file: str) -> None:
+    def upload_file(self, src: str, dst: str) -> None:
         repo = os.getenv('GITHUB_REPOSITORY')
         upload_cmd = ['aws', 's3', 'cp', f'build/{file}', f's3://vv-github-build-artifacts/{repo}/{file}']
 
         if self.RELEASE:
             upload_cmd = ['aws', 's3', 'cp', f'build/{file}', f's3://oneauth-files.vitalvas.dev/release/{file}']
-        
+
         raw = subprocess.Popen(upload_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         raw.wait()
-        
+
         for line in raw.stdout:
             print(line.decode('utf-8').strip())
-        
+
         is_break = False
         for line in raw.stderr:
             print(line.decode('utf-8').strip())
             is_break = True
-    
+
         if is_break:
             sys.exit(1)
 
@@ -216,27 +214,27 @@ class Make:
         print(f'GOOS: {self.GOOS}, GOARCH: {self.GOARCH}')
 
         self.clean()
-        
+
         github_actions = os.getenv('GITHUB_ACTIONS')
 
-        upload_files = []
+        upload_files = {}
 
-        for app in self._apps:
+        for app in self.apps:
             if app_name != 'all' and app_name != app.get('name'):
                 continue
-            
+
             build = app.get('build')
             if build and f'{self.GOOS}/{self.GOARCH}' in build:
-                upload_files.extend(
+                upload_files.update(
                     self.build_app(app)
                 )
             elif not build:
                 if github_actions and f'{self.GOOS}/{self.GOARCH}' == 'linux/amd64':
-                    upload_files.extend(
+                    upload_files.update(
                         self.build_app(app)
                     )
                 elif not github_actions:
-                    upload_files.extend(
+                    upload_files.update(
                         self.build_app(app)
                     )
 
@@ -247,10 +245,10 @@ class Make:
 
 if __name__ == '__main__':
     make = Make()
-    
+
     parser = argparse.ArgumentParser()
-    parser.add_argument('--app', choices=['all'] + [app.get('name') for app in make._apps], default='all')
-    
+    parser.add_argument('--app', choices=['all'] + [app.get('name') for app in make.apps], default='all')
+
     args = parser.parse_args()
-    
+
     make.build(args.app)
