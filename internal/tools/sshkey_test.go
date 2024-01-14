46fb4ee92aeeb290b56ecca3db71c889e6b1f63b
Vitaliy Vasylenko <me@vitalvas.com>
2024-01-13 20:05:52 -0500

    feat: add test for load ssh-key

--- internal/tools/sshkey_test.go
+++ internal/tools/sshkey_test.go
@@ -1,6 +1,10 @@
 package tools
 
 import (
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
 	"testing"
 
 	"golang.org/x/crypto/ssh"
@@ -11,6 +15,52 @@ const (
 	samplePublicKey            = "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEgIBOKTbUjfgXzCNhgrth1lu/5RG5Quplafmzei+clUW107vE02RB3ddxGM94gLB5rEpWpcWYPQUZchTZ7r+w0="
 	samplePublicKeyFingerprint = "SHA256:S20wyk0CtWZRweGzGtmW/G0PdXGK/ZS1YxOJKHwoFT0"
 )
+
+func TestGetSSHPublicKey(t *testing.T) {
+	key, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		t.Fatalf("Failed to generate ecdsa key : %v", err)
+	}
+
+	tests := []struct {
+		name        string
+		inputKey    crypto.PublicKey
+		expectedErr bool
+	}{
+		{
+			name:        "ValidPublicKey",
+			inputKey:    &key.PublicKey,
+			expectedErr: false,
+		},
+		{
+			name:        "InvalidPublicKey",
+			inputKey:    nil,
+			expectedErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			result, err := GetSSHPublicKey(tt.inputKey)
+
+			if !tt.expectedErr && err != nil {
+				t.Fatalf("Expected no error, but got: %v", err)
+			}
+
+			if tt.expectedErr && err == nil {
+				t.Fatalf("Expected error: %v, but got no error", tt.expectedErr)
+			}
+
+			if !tt.expectedErr && err != nil {
+				t.Fatalf("Expected error: %v, but got error: %v", tt.expectedErr, err)
+			}
+
+			if !tt.expectedErr && len(result) == 0 {
+				t.Fatal("Expected non-empty SSH public key")
+			}
+		})
+	}
+}
 
 func TestSSHFingerprint(t *testing.T) {
 	pubKey, _, _, _, err := ssh.ParseAuthorizedKey([]byte(samplePublicKey))
